0cc9396fcf36bc5b34574be3e2efd63bbce7ea08
Ghovindh KG <111132706+Ghovkg02@users.noreply.github.com>
2023-11-09 09:55:38 +0530

    Add files via upload

---
+++ Happy Birthday by Stark.py
@@ -0,0 +1,22 @@
+from colorama import Fore, Back, Style, init
+import time
+def unique_birthday_wish(name):
+    message = f"{Fore.RED}{Style.BRIGHT}Happy Birthday, {name}!{Style.RESET_ALL}"
+    unique_part = f"{Fore.MAGENTA}{Style.BRIGHT}May your day be filled with joy, laughter, and unforgettable memories, just like the beautiful moments we've shared together. You're not just a year older, but a year wiser, kinder, and more amazing than ever. Let this year be your best one yet, full of success and new adventures GOVIND.IO LEXXX GOOO. 🎂🎉🎁{Style.RESET_ALL}"
+    return f"{message}\n{unique_part}"
+recipient_name = "Ghovindh"
+init(autoreset=True)
+for _ in range(20):
+    print(unique_birthday_wish(recipient_name))
+    time.sleep(0.5)
+from colorama import Fore, Style, init
+import pyfiglet
+def unique_birthday_wish(name):
+    wish_message = f"{Fore.CYAN}{Style.BRIGHT}{pyfiglet.figlet_format('Happy Birthday!')}{Style.RESET_ALL}"
+    unique_part = f"{Fore.MAGENTA}{Style.BRIGHT}May your day be filled with joy, laughter, and unforgettable memories, just like the beautiful moments we've shared together. You're not just a year older, but a year wiser, kinder, and more amazing than ever. Let this year be your best one yet, full of success and new adventures,BUG BOUNTY NEEDS TO BE DONE LOL . 🎂🎉🎁{Style.RESET_ALL}"
+    return f"{wish_message}\n{unique_part}"
+recipient_name = "Ghovindh"
+init(autoreset=True)
+for _ in range(20):
+    print(unique_birthday_wish(recipient_name))
+    time.sleep(0.5)
